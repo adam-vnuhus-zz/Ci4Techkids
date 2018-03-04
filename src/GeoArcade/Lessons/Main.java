1e752704fcd1ddb8d51d522e81f0aba6e1d6aab9
Tuan Anh <tuananh1108.97@gmail.com>
2018-03-04 20:30:48 +0700

    Lesson 03-Mar-18

--- src/GeoArcade/Lessons/Main.java
+++ src/GeoArcade/Lessons/Main.java
@@ -2,6 +2,8 @@ package GeoArcade.Lessons;
 
 public class Main {
     public static void main(String[] args) {
+        /*new GameWindow khoi tao mot object GameWindow
+        * GameWindow gameWindow tao ra mot con tro de tro den vung nho GameWindow*/
         GameWindow gameWindow = new GameWindow();
         gameWindow.gameLoop();
     }
