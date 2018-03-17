fa2d5cbba48ae465e772c906d17c9dd581015a25
Tuan Anh <tuananh1108.97@gmail.com>
2018-03-17 12:47:07 +0700

    HW 15_Mar

--- src/game/enemy/bullet/EnemyShoot.java
+++ src/game/enemy/bullet/EnemyShoot.java
@@ -4,17 +4,27 @@ import base.FrameCounter;
 import base.GameObject;
 import base.GameObjectManager;
 import game.enemy.Enemy;
+import game.player.Player;
 
 public class EnemyShoot {
 
     private FrameCounter frameCounter = new FrameCounter(50);
+    private Player player;
+
+    public EnemyShoot(){
+        this.player = new Player();
+    }
 
     public void run(Enemy enemy) {
         if (this.frameCounter.run()) {
             BulletEnemy bulletEnemy = new BulletEnemy();
             bulletEnemy.position.set(enemy.position);
-            bulletEnemy.velocity.set(0, 4);
+            bulletEnemy.velocity.set(0, 1);
+            bulletEnemy.velocity.set(GameObjectManager.instance.getPlayer().position.subtract(bulletEnemy.position));
+            bulletEnemy.velocity.set(bulletEnemy.velocity.normalize().multiply(5));
+
             GameObjectManager.instance.add(bulletEnemy);
+
         }
     }
 }
